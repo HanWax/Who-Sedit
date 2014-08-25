7e80cc5f2ab3da2654b939aca7043790ebd8d684
Hannah Waxman <hannah.waxman8@gmail.com>
2014-08-25 20:35:06 +0100

    Added post model

---
+++ spec/features/post_feature_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper' 
+
+describe 'posts' do 
+	context 'no posts' do 
+		it 'should display no posts' do
+			visit '/posts'
+			expect(page).to have_content('No posts yet')
+		end 
+	end 
+
+	context 'when there are posts' do 
+		it 'should display the posts' do 
+			visit '/posts'
+			click_link 'Add post' 
+			post = create(:post)
+			click_button 'Create post'
+			expect(page).to have_content('Hello world')
+			expect(page).to have_content('Hi')
+		end 
+	end 
+end 
